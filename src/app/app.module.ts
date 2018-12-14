66ed0a2e60f98f444b504c767c52ef6f79bae9be
sgtmms@gmail.com <sgtmms@gmail.com>
2018-12-13 16:21:23 -0800

    added bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooComponent
+    FooComponent,
+    BarComponent
   ],
   imports: [
     BrowserModule,
