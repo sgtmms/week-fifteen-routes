f39e774025a5bffdd60a1a25d5715e39effbd97d
sgtmms <LMIMS1@madisoncollege,edu>
2018-12-16 14:34:05 -0600

    pnf added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FooComponent } from './foo/foo.component';
 import { BarComponent } from './bar/bar.component';
 import { CatComponent } from './cat/cat.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'foo', component: FooComponent },
+  { path: 'bar', component: BarComponent },
+  { path: 'cat', component: CatComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
+ 
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   declarations: [
     AppComponent,
     FooComponent,
     BarComponent,
-    CatComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    CatComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
